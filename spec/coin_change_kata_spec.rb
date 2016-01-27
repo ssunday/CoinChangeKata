ff276f7fb055e00494dd873727073cc2f17e3ae4
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-01-27 08:34:32 -0600

    Added more tests

--- spec/coin_change_kata_spec.rb
+++ spec/coin_change_kata_spec.rb
@@ -5,24 +5,28 @@ describe CoinChangeKata do
     @coin_change = CoinChangeKata.new
   end
 
-  it "returns a penny for a penny" do
-    expect(@coin_change.change(1)).to eq [1]
-  end
+  describe "#change" do
 
-  it "returns two pennies for 2 cents" do
-    expect(@coin_change.change(2)).to eq [1,1]
-  end
+    it "returns 1 penny for 1 cent" do
+      expect(@coin_change.change(1)).to eq [1]
+    end
 
-  it "returns a nickel and a penny for 6 cents" do
-    expect(@coin_change.change(6)).to eq [5,1]
-  end
+    it "returns 2 pennies for 2 cents" do
+      expect(@coin_change.change(2)).to eq [1,1]
+    end
 
-  it "returns a dime for 10 cents" do
-    expect(@coin_change.change(10)).to eq [10]
-  end
+    it "returns 1 nickle, 1 penny for 6 cents" do
+      expect(@coin_change.change(6)).to eq [5,1]
+    end
 
-  it "returns a quarter and 2 pennies for 27 cents" do
-    expect(@coin_change.change(27)).to eq [25,1,1]
+    it "returns 1 quarter, 1 dime, 1 nicklel for 40 cents" do
+      expect(@coin_change.change(40)).to eq [25,10,5]
+    end
+
+    it "returns 3 quarters, 1 nicklel, 3 pennies, for 83 cents" do
+      expect(@coin_change.change(83)).to eq [25,25,25,5,1,1,1]
+    end
+
   end
 
 end
